d46865c952647652f29e8f6a1ab410488bc56df5
Youssef-Ahmed-Zahran <youssefzahran480@gmail.com>
2026-03-04 04:18:03 +0200

    edit cart model

--- api/src/features/cart/models/cart.model.js
+++ api/src/features/cart/models/cart.model.js
@@ -1,5 +1,25 @@
 import mongoose from "mongoose";
 
+const cartItemSchema = new mongoose.Schema({
+  product: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Product",
+    required: true,
+  },
+  quantity: {
+    type: Number,
+    required: true,
+    min: 1,
+    max: [100, "Quantity cannot exceed 100"],
+    default: 1,
+  },
+  price: {
+    type: Number,
+    required: true,
+    min: 0,
+  },
+});
+
 const cartSchema = new mongoose.Schema(
   {
     user: {
@@ -8,27 +28,7 @@ const cartSchema = new mongoose.Schema(
       required: true,
       unique: true,
     },
-    items: [
-      {
-        product: {
-          type: mongoose.Schema.Types.ObjectId,
-          ref: "Product",
-          required: true,
-        },
-        quantity: {
-          type: Number,
-          required: true,
-          min: 1,
-          max: [100, "Quantity cannot exceed 100"],
-          default: 1,
-        },
-        price: {
-          type: Number,
-          required: true,
-          min: 0,
-        },
-      },
-    ],
+    items: [cartItemSchema],
   },
   { timestamps: true }
 );
