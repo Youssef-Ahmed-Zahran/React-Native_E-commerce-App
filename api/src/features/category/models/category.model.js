53d629953492486bf3400eabfd36be2d29af07f0
Youssef-Ahmed-Zahran <youssefzahran480@gmail.com>
2026-03-04 02:05:31 +0200

    create category model

---
+++ api/src/features/category/models/category.model.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const categorySchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: true,
+      maxLength: 32,
+      unique: true,
+    },
+    image: {
+      type: String,
+      default: "",
+    },
+  },
+  { timestamps: true }
+);
+
+export const Category = mongoose.model("Category", categorySchema);
