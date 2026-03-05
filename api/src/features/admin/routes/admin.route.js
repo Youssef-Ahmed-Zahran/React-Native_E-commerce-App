fcf65c0d6f970760a3f4d23c38b09c3d97c5a9dc
Youssef-Ahmed-Zahran <youssefzahran480@gmail.com>
2026-03-05 16:15:20 +0200

    create admin route

---
+++ api/src/features/admin/routes/admin.route.js
@@ -0,0 +1,40 @@
+import { Router } from "express";
+import {
+  createProduct,
+  getAllCustomers,
+  getAllOrders,
+  getAllProducts,
+  getDashboardStats,
+  updateOrderStatus,
+  updateProduct,
+  deleteProduct,
+  createCategory,
+  getAllCategories,
+  updateCategory,
+  deleteCategory,
+} from "../controllers/admin.controller.js";
+import { verifyTokenAndAdmin } from "../../../middlewares/verifyToken.middleware.js";
+
+const router = Router();
+
+// optimization - DRY
+router.use(verifyTokenAndAdmin);
+
+router.post("/products", createProduct);
+router.get("/products", getAllProducts);
+router.put("/products/:id", updateProduct);
+router.delete("/products/:id", deleteProduct);
+
+router.post("/categories", createCategory);
+router.get("/categories", getAllCategories);
+router.put("/categories/:id", updateCategory);
+router.delete("/categories/:id", deleteCategory);
+
+router.get("/orders", getAllOrders);
+router.patch("/orders/:orderId/status", updateOrderStatus);
+
+router.get("/customers", getAllCustomers);
+
+router.get("/stats", getDashboardStats);
+
+export default router;
